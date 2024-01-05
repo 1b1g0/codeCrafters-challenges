1601e2484a26b2dc6b3aa05ea1db9d17c04c6953
unknown <igorbrunossi2@gmail.com>
2024-01-05 06:50:50 -0300

    stage 2

--- app/main.js
+++ app/main.js
@@ -5,20 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 
-// Accept a TCP connection
-// Read data from the connection (we'll get to parsing it in later stages)
-// Respond with HTTP/1.1 200 OK\r\n\r\n (there are two \r\ns at the end)
-// HTTP/1.1 200 OK is the HTTP Status Line.
-// \r\n, also known as CRLF, is the end-of-line marker that HTTP uses.
-// The first \r\n signifies the end of the status line.
-// The second \r\n signifies the end of the response headers section (which is empty in this case).
-
 const server = net.createServer((socket) => {
     // 'ouvindo' conexoes
     console.log('Conectado com sucesso.')   
 
     socket.on('data', (data) => {
-        console.log(typeof data)
+        console.log(data)
         socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
         
     });
