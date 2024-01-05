c8b04380625b7d1c1a51ac39801520d73871540b
unknown <igorbrunossi2@gmail.com>
2024-01-05 18:11:48 -0300

    stage 6 tst

--- app/main.js
+++ app/main.js
@@ -1,17 +1,19 @@
 //@ts-check
 
 const net = require("net");
+const { readdir, } = require("fs/promises");
 const CRLF = `\r\n\r\n`;
 const lineSep = `\r\n`;
-const args = process.argv.slice(1);
-
+// todo: parse args
+const args = process.argv.slice(2);
+const filePath = args[1];
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-const getBody = (path, reqHeader) => {
+const getBody = (path, reqHeader, files) => {
     const contentType = 'Content-Type: text/plain';
-    if (path.length < 2){
+    if (path.length < 2) {
         return CRLF;
     }
     
@@ -32,11 +34,25 @@ const getBody = (path, reqHeader) => {
         return `${lineSep}${contentType}${lineSep}Content-Length: ${contentLen}${CRLF}${userAgent}`;
     }
     
+    if (path.match('/files')) {
+        
+        const contentTypeApp = 'Content-Type: application/octet-stream';
+        const filename = path.slice(7);
+        
+        const found = files.find(file => file == filename);
+        if (!found) {
+            return false;
+        } else {
+            
+            return `${lineSep}${contentTypeApp}${lineSep}Content-Length: ${found.length}${CRLF}${found}`;
+        }
+    }
 }
 const server = net.createServer(async (socket) => {
     // 'ouvindo' conexoes
     console.log('Conectado com sucesso.', args)   
     try {
+        const files = await readdir(filePath);
         await socket.on('data', (data) => {
             const headers = data.toString().split(`\r\n`, 3);
             
@@ -44,11 +60,15 @@ const server = net.createServer(async (socket) => {
             const path = startLine[1];
             const version = startLine[2];
 
-            const body = getBody(path, headers);
+            const body = getBody(path, headers, files);
             const res200 = `${version} 200 OK${body}`;
             const res404 = `${version} 404 Not Found${CRLF}`;
             
-            path.length < 2 || path.match('/echo') || path.match('/user-agent') ? socket.write(res200) : socket.write(res404);
+            path.length < 2 ||
+            path.match('/echo') ||
+            path.match('/user-agent') ||
+            body ?
+            socket.write(res200) : socket.write(res404);
         });
 
         socket.on("close", () => {
