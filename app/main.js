1859c8067457f8a6c1fa22fb2eb7ed7e6f95912c
unknown <igorbrunossi2@gmail.com>
2024-01-05 16:14:37 -0300

    stage3

--- app/main.js
+++ app/main.js
@@ -1,28 +1,44 @@
 const net = require("net");
+const CRLF = `\r\n\r\n`;
 //@ts-check
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-
+const getBody = (path, content) => {
+    const contentType = 'Content-Type: text/plain';
+    const fill = path.split('/');
+    const bContent = fill[(fill.length - 1)]
+    const contentLen = bContent.length;
+    return `\r\n${contentType}\r\nContent-Length: ${contentLen}${CRLF}${bContent}`;
+}
 const server = net.createServer((socket) => {
     // 'ouvindo' conexoes
     console.log('Conectado com sucesso.')   
 
     socket.on('data', (data) => {
-        const CRLF = `\r\n\r\n`;
-        const headers = data.toString().split(' ');
-        const method = headers[0];
-        const path = headers[1];
-        const version = headers[2].slice(0,8);
+        
+        const lineSep = `\r\n`;
+        const headers = data.toString().split(`\r\n`, 3);
+        console.log(headers)
+
+        const startLine = headers[0].split(' ', 3);
+        const method = startLine[0];
+        const path = startLine[1];
+        const version = startLine[2];
         console.log(`Method: ${method}\nPath: ${path}\nVesion: ${version}`)
+       
+        if (path.length < 2) {
+            socket.write(`${version} 200 OK${CRLF}`)
+        };
 
-        const res200 = `${version} 200 OK${CRLF}`;
+        const body = getBody(path);
+        const res200 = `${version} 200 OK${body}`;
         const res404 = `${version} 404 Not Found${CRLF}`;
-
-        path == '/' ? socket.write(res200) : socket.write(res404);
         
+        //console.log(res200)
         
+        path.match('/echo') ? socket.write(res200) : socket.write(res404);
     });
 
     socket.on("close", () => {
